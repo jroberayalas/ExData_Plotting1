e468aa4804e2b37a399309b4d75bdbd0cf7fb2fa
ichbinjras <ichbinjras@gmail.com>
2014-10-10 17:44:06 +0100

    Comments
    Added comments to plot1.R

--- plot1.R
+++ plot1.R
@@ -1,11 +1,15 @@
 rm(list = ls())
 
+# sqldf <- for efficient reading of tables
 library(sqldf)
+
+# Read data with only the required rows
 data <- read.csv.sql("Data/household_power_consumption.txt", 
                      sql = "SELECT * from file WHERE Date in ('1/2/2007', '2/2/2007') ", 
                      sep = ";", 
                      header = TRUE)
 
+# Plot the histogram
 png(file = "ExData_Plotting1/plot1.png", bg = "transparent")
 hist(data$Global_active_power, 
      col = "red",
