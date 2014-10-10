e9a8b2a92660f018ce0466a9d899b9d243ac793b
ichbinjras <ichbinjras@gmail.com>
2014-10-10 17:16:19 +0100

    Add plot1
    Code and figure for plot1

---
+++ plot1.R
@@ -0,0 +1,14 @@
+rm(list = ls())
+
+library(sqldf)
+data <- read.csv.sql("Data/household_power_consumption.txt", 
+                     sql = "SELECT * from file WHERE Date in ('1/2/2007', '2/2/2007') ", 
+                     sep = ";", 
+                     header = TRUE)
+
+png(file = "plot1.png", bg = "transparent")
+hist(data$Global_active_power, 
+     col = "red",
+     main = "Global Active Power",
+     xlab = "Global Active Power (kilowatts)")
+dev.off()
